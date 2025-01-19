e16e6b67cc7cecfebab41ef3626404a7b22ad89a
FaezaAldarweesh <faeza.aldarweesh@gmail.com>
2025-01-19 20:21:13 +0200

    update trip resources

--- app/Http/Resources/AllTripResources.php
+++ app/Http/Resources/AllTripResources.php
@@ -19,7 +19,7 @@ class AllTripResources extends JsonResource
             'trip id' => $this->id,
             'trip name' => $this->name == 'delivery' ? 'توصيل' : 'مدرسية', 
             'trip type' => $this->type == 'go' ? 'ذهاب' : 'عودة', 
-            'trip bus' => $this->bus->name,
+            'trip path' => $this->path->name, 
             'trip status' => $this->status == 0 ? 'منتهية' : 'جارية',
             'trip start_date' =>  $this->formatTimeToArabic($this->start_date),
             'trip end_date' => $this->formatTimeToArabic($this->end_date),
