727e5b412d86bd1b653be7d49e253d263600c60b
RimaKi <rimakiwan1999@gmail.com>
2025-01-23 21:38:40 +0300

    feat| TripTrackController.php TripTrackSeeder.php map.blade.php api.php web.php|Add a trip path tracking feature on the map for admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Path;
 use App\Models\Trip;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,7 @@ use App\Http\Controllers\BladeController\StudentController;
 use App\Http\Controllers\BladeController\CheckOutController;
 use App\Http\Controllers\BladeController\EmployeeController;
 use App\Http\Controllers\BladeController\TransportController;
+use \App\Http\Controllers\BladeController\TripTrackController;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,49 +41,49 @@ Auth::routes(['register' => false]);
 Route::group(['middleware' => ['auth']], function () {
 
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    
+
     Route::resource('student',  StudentController::class);
     Route::get('all_trashed_student', [StudentController::class, 'all_trashed_student'])->name('all_trashed_student');
     Route::get('restore_student/{student_id}', [StudentController::class, 'restore'])->name('restore_student');
     Route::delete('forceDelete_student/{student_id}', [StudentController::class, 'forceDelete'])->name('forceDelete_student');
-    
-    Route::Resource('driver',DriverController::class); 
+
+    Route::Resource('driver',DriverController::class);
     Route::get('all_trashed_driver', [DriverController::class, 'all_trashed_driver'])->name('all_trashed_driver');
     Route::get('restore_driver/{driver_id}', [DriverController::class, 'restore'])->name('restore_driver');
     Route::delete('forceDelete_driver/{driver_id}', [DriverController::class, 'forceDelete'])->name('forceDelete_driver');
-    
-    Route::Resource('bus',BusController::class); 
+
+    Route::Resource('bus',BusController::class);
     Route::get('all_trashed_bus', [BusController::class, 'all_trashed_bus'])->name('all_trashed_bus');
     Route::get('restore_bus/{bus_id}', [BusController::class, 'restore'])->name('restore_bus');
     Route::delete('forceDelete_bus/{bus_id}', [BusController::class, 'forceDelete'])->name('forceDelete_bus');
-    
-    Route::Resource('path',PathController::class); 
+
+    Route::Resource('path',PathController::class);
     Route::get('all_trashed_path', [PathController::class, 'all_trashed_path'])->name('all_trashed_path');
     Route::get('restore_path/{path_id}', [PathController::class, 'restore'])->name('restore_path');
     Route::delete('forceDelete_path/{path_id}', [PathController::class, 'forceDelete'])->name('forceDelete_path');
-    
-    Route::Resource('station',StationController::class); 
+
+    Route::Resource('station',StationController::class);
     Route::get('all_trashed_station', [StationController::class, 'all_trashed_station'])->name('all_trashed_station');
     Route::get('restore_station/{station_id}', [StationController::class, 'restore'])->name('restore_station');
     Route::delete('forceDelete_station/{station_id}', [StationController::class, 'forceDelete'])->name('forceDelete_station');
     Route::post('update_station_status/{station_id}',[StationController::class,'update_station_status'])->name('update_station_status');
-    
-    Route::Resource('trip',TripController::class); 
+
+    Route::Resource('trip',TripController::class);
     Route::get('all_trashed_trip', [TripController::class, 'all_trashed_trip'])->name('all_trashed_trip');
     Route::get('restore_trip/{trip_id}', [TripController::class, 'restore'])->name('restore_trip');
     Route::delete('forceDelete_trip/{trip_id}', [TripController::class, 'forceDelete'])->name('forceDelete_trip');
     Route::post('update_trip_status/{trip_id}/', [TripController::class, 'update_trip_status'])->name('update_trip_status');
     Route::get('all_student_trip/{trip_id}', [TripController::class, 'all_student_trip'])->name('all_student_trip');
-    Route::post('update_student_status/{student_id}/{trip_id}', [TripController::class, 'update_student_status'])->name('update_student_status');  
-    Route::post('accept_student_transport/{transport_id}', [TripController::class, 'update_student_status_transport'])->name('accept_student_transport');  
-    Route::put('update_student_time_arrive/{student_id}/{trip_id}', [TripController::class, 'update_student_time_arrive'])->name('update_student_time_arrive');    
+    Route::post('update_student_status/{student_id}/{trip_id}', [TripController::class, 'update_student_status'])->name('update_student_status');
+    Route::post('accept_student_transport/{transport_id}', [TripController::class, 'update_student_status_transport'])->name('accept_student_transport');
+    Route::put('update_student_time_arrive/{student_id}/{trip_id}', [TripController::class, 'update_student_time_arrive'])->name('update_student_time_arrive');
 
-    
-    Route::resource('employee', EmployeeController::class); 
+
+    Route::resource('employee', EmployeeController::class);
     Route::get('all_trashed_employee',[EmployeeController::class,'all_trashed_employee']) -> name("all_trashed_employee");
     Route::get('restor_employee/{employee_id}',[EmployeeController::class,'restore']) -> name("restore_employee");
     Route::delete('forceDelete/{employee_id}',[EmployeeController::class,'forceDelete']) -> name("forceDelete_employee");
-    
+
     Route::resource('users', UserController::class);
     Route::get('all_trushed_user',[UserController::class,'all_trushed_user'])->name("all_trashed_user");
     Route::get('restore_user/{user_id}',[UserController::class,'restore'])->name('restore_user');
@@ -96,13 +98,16 @@ Route::group(['middleware' => ['auth']], function () {
 
     Route::get('/get-trip-stations/{trip}', function ($tripId) {
         $trip = Trip::with('path.stations')->findOrFail($tripId); // تعديل العلاقة لتشمل stations
-        $stations = $trip->path->stations; 
+        $stations = $trip->path->stations;
         return response()->json(['stations' => $stations]);
     });
 
-   Route::post('update_student_status_transport/{student_id}', [TransportController::class, 'store'])->name('update_student_status_transport');  
-   Route::get('transport', [TransportController::class, 'index'])->name('transport');  
-   Route::delete('destroy_transport/{transport_id}', [TransportController::class, 'destroy'])->name('destroy_transport');  
+   Route::post('update_student_status_transport/{student_id}', [TransportController::class, 'store'])->name('update_student_status_transport');
+   Route::get('transport', [TransportController::class, 'index'])->name('transport');
+   Route::delete('destroy_transport/{transport_id}', [TransportController::class, 'destroy'])->name('destroy_transport');
+
+    Route::get('/trip-tracks/{trip}/map',[TripTrackController::class,'showMap'])->middleware('role:Admin');
+    Route::get('/trip-tracks/{trip}',[TripTrackController::class,'show'])->middleware('role:Admin');
 
 });
 
