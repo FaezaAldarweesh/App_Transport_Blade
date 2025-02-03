92d5967318b834d85185b5494d74f81df909cfa5
FaezaAldarweesh <faeza.aldarweesh@gmail.com>
2025-02-03 12:01:19 +0200

    update conflict

--- app/Services/ApiServices/TripService.php
+++ app/Services/ApiServices/TripService.php
@@ -2,12 +2,14 @@
 
 namespace App\Services\ApiServices;
 
-use App\Http\Traits\AllStudentsByTripTrait;
-use App\Http\Traits\ApiResponseTrait;
-use App\Models\Trip;
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Auth;
+use App\Models\Trip;
+use App\Models\StudentTrip;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Traits\ApiResponseTrait;
+use App\Http\Traits\AllStudentsByTripTrait;
 
 class TripService
 {
@@ -106,56 +108,62 @@ class TripService
             return $this->failed_Response($th->getMessage(), 400);
         }
     }
-
     //========================================================================================================================
     public function update_trip_status($trip_id)
     {
         try {
-
             $Trip = Trip::find($trip_id);
             $user = Auth::user();
-
+            
+            if (!$Trip) {
+                throw new \Exception('الرحلة غير موجودة');
+            }
+    
             if ($user->role == 'supervisor') {
                 $Trip = $user->trips()->find($trip_id);
-
-                if (!$Trip) {
-                    throw new \Exception('لا يمكنك التعديل على حالة رحلة غير مخصصة لك ');
+                             
+                if(!$Trip){
+                    throw new \Exception('لا يمكنك التعديل على حالة رحلة غير مخصصة لك');
                 }
-
             }
-
-            $Trip->status = !$Trip->status;
-
+    
+            $Trip->status = $Trip->status == 1 ? 0 : 1;
+    
             if ($Trip->status == 0) {
-                // جلب الطلاب المرتبطين بالرحلة مع حالة غياب أو منقول
-                $Trip->students()
-                    ->wherePivotIn('status', ['absent', 'Moved_to'])
-                    ->each(function ($student) use ($Trip) {
-                        $Trip->students()->updateExistingPivot($student->id, ['status' => 'attendee']);
-                    });
-
-                // حذف الطلاب المرتبطين بالرحلة مع حالة نقل
-                $Trip->students()->wherePivot('status', 'Transferred_from')
-                    ->detach();
-
-                $Trip->path->stations()->update(['status' => 0]);
-            } else {
-                //منع المشرفة من بدأ أكثر من رحلة في نفس الوقت
-                $activeTrip = $user->trips()->where('status', 1)->where('trips.id', '!=', $trip_id)->first();
-                if ($activeTrip) {
-                    throw new \Exception('لا يمكنك بدء هذه الرحلة لأن هناك رحلة أخرى لا تزال قيد التنفيذ.');
+                // تحديث حالة الطلاب المرتبطين بالرحلة
+                $students = $Trip->students()->wherePivotIn('status', ['absent', 'Moved_to'])->get();
+                foreach ($students as $student) {
+                    $Trip->students()->updateExistingPivot($student->id, ['status' => 'attendee']);
                 }
+    
+                // حذف الطلاب المرتبطين بالرحلة مع حالة نقل
+                $Trip->students()->wherePivot('status', 'Transferred_from')->detach();
+    
+                // تحديث حالة المحطات إلى 0
+                $Trip->path->stations()->update(['status' => 0]);
+    
+                $Trip->save(); 
+    
+                return ['trip' => $Trip, 'message' => 'تمت عملية إنهاء الرحلة بنجاح'];
+            } else {
+                // منع المشرفة من بدء أكثر من رحلة في نفس الوقت
+                if ($user->trips()->exists()) {
+                    $activeTrip = $user->trips()->where('status', 1)->where('trips.id', '!=', $trip_id)->first();
+                    if ($activeTrip) {
+                        throw new \Exception('لا يمكنك بدء هذه الرحلة لأن هناك رحلة أخرى لا تزال قيد التنفيذ.');
+                    }
+                }
+                
+                $Trip->save(); 
+                (new NotificationService())->tripNotification($Trip);
+                return ['trip' => $Trip, 'message' => 'تمت عملية بدأ الرحلة بنجاح'];
             }
-
-            $Trip->save();
-            (new NotificationService())->tripNotification($Trip);
-            return $Trip;
-
-        } catch (\Exception $e) {
-            Log::error($e->getMessage());
+    
+        } catch (\Exception $e) { 
+            Log::error($e->getMessage()); 
             return $this->failed_Response($e->getMessage(), 404);
-        } catch (\Throwable $th) {
-            Log::error($th->getMessage());
+        } catch (\Throwable $th) { 
+            Log::error($th->getMessage()); 
             return $this->failed_Response('Something went wrong with update status Trip', 400);
         }
     }
@@ -164,11 +172,13 @@ class TripService
     public function all_student_trip($trip_id)
     {
         try {
-            $trip = Trip::find($trip_id);
-            $trip->load('students');
-
-            return $trip;
-
+            $trip = Trip::findOrFail($trip_id);
+            $trip->load(['students' => function ($query) {
+                $query->orderBy('student_trip.time_arrive', 'asc'); 
+            }]);
+    
+            return $trip; 
+    
         } catch (\Throwable $th) {
             Log::error($th->getMessage());
             return $this->failed_Response('Something went wrong with get all students', 400);
@@ -190,23 +200,46 @@ class TripService
         }
     }
     //========================================================================================================================
-
     /**
      * method to show Trip alraedy exist
      * @param  $Trip_id
      * @return /Illuminate\Http\JsonResponse if have an error
      */
-    public function details_Trip($Trip_id)
-    {
-        try {
+    public function details_Trip($Trip_id) {
+        try {    
             $Trip = Trip::find($Trip_id);
-            $Trip->load('students', 'users', 'drivers', 'path.stations');
+            $Trip->load('students','users','drivers','path.stations');
 
             return $Trip;
-        } catch (\Throwable $th) {
-            Log::error($th->getMessage());
-            return $this->failed_Response('Something went wrong with show Trip', 400);
-        }
+        } catch (\Throwable $th) { Log::error($th->getMessage()); return $this->failed_Response('Something went wrong with show Trip', 400);}
+    }
+    //========================================================================================================================
+    public function update_student_status($student_id,$trip_id)
+    {
+        try {
+            $student = StudentTrip::where('student_id',$student_id)->where('trip_id',$trip_id)->first();
+
+            $user = Auth::user();
+            $trip = Trip::where('id',$student->trip_id)->first();
+            
+            if($user->role == 'parent' && $trip->status == 1 ){
+                throw new \Exception( 'لا يمكنك تعديل حالة الطالب في حال كانت الرحلة حاليا جارية');
+            }
+            
+            $status = $student->status == 'attendee' ? 'absent' : 'attendee';
+            $student->update(['status' => $status]); 
+
+            return $trip;
+        } catch (\Exception $e) { Log::error($e->getMessage()); return $this->failed_Response($e->getMessage(), 404);
+        } catch (\Throwable $th) { Log::error($th->getMessage()); return $this->failed_Response('Something went wrong with update student status', 400);}
+    }
+    //========================================================================================================================
+    public function trip_filter(Request $request){
+        try {
+            $Trips = Trip::filter($request)->orderBy('start_date', 'asc')->get();
+            $Trips->load('users','drivers');
+            return $Trips;
+        } catch (\Throwable $th) { Log::error($th->getMessage()); return $this->failed_Response('Something went wrong with fetche Trips', 400);}
     }
     //========================================================================================================================
 }
