d1d691fa2205dbfd9da296cd8755fd460fdab988
RimaKi <rimakiwan1999@gmail.com>
2025-02-19 19:48:31 +0300

    edit auth broadcast

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class BroadcastServiceProvider extends ServiceProvider
@@ -12,7 +13,14 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // تسجيل مسارات البث لـ Web بدون JWT
         Broadcast::routes();
+
+        // تسجيل مسارات البث لـ API مع JWT
+        Broadcast::routes([
+            'middleware' => ['auth:api'],
+            'prefix' => 'api' // تأكد من أن المسار يتم توجيهه إلى `/api/broadcasting/auth`
+        ]);
 
         require base_path('routes/channels.php');
     }
